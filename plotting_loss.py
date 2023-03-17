1aa1d34934f22d6346590b2458071de213220312
natbishay <46910864+natbishay@users.noreply.github.com>
2023-03-17 14:45:04 -0700

    plotting code and images

--- plotting_loss.py
+++ plotting_loss.py
@@ -7,20 +7,23 @@ import os
 # plt.show()
 
 directory = "log"
+experiments = ["03_14_2023","03_15_2023"]
+titles = ["Negative Rank", "Negative Rank with Shared Layer and Scaled Loss"]
 
 names = ["neg_rank", "paraphrase", "sentiment","similarity"]
 leg = []
-
-for i, filename in enumerate(os.listdir(directory)):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        for i in range(len(names)):
-            if filename.__contains__(names[i]):
-                leg.append(names[i])
-                data = np.load(f)
-                print(data)
-                plt.plot(data)
-
-plt.legend(leg)
-plt.show()
+for j, experiment in enumerate(experiments):
+    for i, filename in enumerate(os.listdir(directory)):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            for i in range(len(names)):
+                if filename.__contains__(names[i]) and filename.__contains__(experiment):
+                    leg.append(names[i])
+                    data = np.load(f)
+                    plt.plot(data)
+    
+    plt.title(titles[j])
+    plt.legend(leg)
+    plt.savefig(titles[j]+".jpg")
+    plt.figure()
